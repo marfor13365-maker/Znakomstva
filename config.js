d717c23af118406de6c9015483f09d234a1a3398
marfor13365-maker <Vektorizi@yandex.ru>
2026-06-14 17:22:15 +0300

    Fix anon key

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 const SUPABASE_URL = "https://zxzhnmwksygozwtdymmt.supabase.co"
-const SUPABASE_KEY = "sb_publishable_TgcHyaUvfbovHQv2tVi_qA_TzJh-_oj"
+const SUPABASE_KEY = "ВСТАВЬ_СКОПИРОВАННЫЙ_КЛЮЧ"
