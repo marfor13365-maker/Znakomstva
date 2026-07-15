c23eee2ae601a14f5bbd817e409cf26df59071cf
marfor13365-maker <marfor13365@gmail.com>
2026-07-15 12:50:45 +0300

    Update theme.js

--- theme.js
+++ theme.js
@@ -1,3 +1,13 @@
+// ============ ЯНДЕКС.МЕТРИКА ============
+(function(m,e,t,r,i,k,a){
+    m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+    m[i].l=1*new Date();
+    for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+    k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+})(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=110760396', 'ym');
+
+ym(110760396, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
+
 // Общий модуль темы для всех страниц Blizko
 // Подключается как <script src="theme.js"></script> ПЕРЕД остальными скриптами
 
